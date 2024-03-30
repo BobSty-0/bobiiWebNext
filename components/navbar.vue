1ec2a0e4e654875ed81bacd319d637082b2e0823
BobSty <78264479+BobSty-0@users.noreply.github.com>
2024-03-30 21:06:35 +0100

    init

---
+++ components/navbar.vue
@@ -0,0 +1,55 @@
+<style lang="css" scoped>
+@import "../css/components/navbar.css";
+@import "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0";
+</style>
+
+<template>
+  <header>
+    <nav class="navbar">
+      <a class="logo" href="#">Bobii</a>
+      <ul class="menu-links">
+        <span id="close-menu-btn" class="material-symbols-outlined" @click="closeMobileMenu">close</span>
+        <li><a href="/">Home</a></li>
+        <li><a href="#">Dashboard</a></li>
+        <li><a href="#">Documentation</a></li>
+        <li><a href="/premium">Premium</a></li>
+        <li id="loginContainer"><a :href="discordAuth2Link">Login</a></li>
+        <li id="avatarContainer"><img id="avatar" style="display: hidden"></li>
+      </ul>
+      <span id="hamburger-btn" class="material-symbols-outlined" @click="toggleMobileMenu">menu</span>
+    </nav>
+  </header>
+</template>
+
+<script>
+import { discordAuth2Link } from '~/globals';
+
+export default {
+  methods: {
+    toggleMobileMenu() {
+      // Referenz auf das header-Element
+      const header = this.$refs.header;
+      const hamburgerBtn = this.$refs.hamburgerBtn;
+      // Toggle mobile menu on hamburger button click
+      header.classList.toggle("show-mobile-menu");
+    },
+    closeMobileMenu() {
+      // Referenz auf das header-Element
+      const header = this.$refs.header;
+      // Referenz auf das hamburgerBtn-Element
+      const hamburgerBtn = this.$refs.hamburgerBtn;
+      // Referenz auf das closeMenuBtn-Element
+      const closeMenuBtn = this.$refs.closeMenuBtn;
+
+      // Close mobile menu on close button click
+      hamburgerBtn.click();
+    }
+  },
+
+  data() {
+    return {
+      discordAuth2Link: discordAuth2Link
+    };
+  }
+}
+</script>
---
+++ globals.js
@@ -0,0 +1,6 @@
+export const discordAuth2Link = 'https://discord.com/api/oauth2/authorize?client_id=777971396931878912&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fcode%2F&response_type=code&scope=identify+guilds+email';
+export const clientId="777971396931878912"
+export const clientSecret="9r0_oTiTJUblNokjnlsfOdw8IPyvhMaQ"
+export const sessionSecret=")H(J93h_4t09U1efr_"
+export const dbUsername="username"
+export const dbPassword="password"
