37260b93a87d49916e0a0a64286d40d3559310b2
BobSty <78264479+BobSty-0@users.noreply.github.com>
2024-04-04 19:49:34 +0200

    UrlParameter /serverId/channelId eingebaut

---
+++ pages/dashboard/[servId]/[channelId].vue
@@ -0,0 +1,21 @@
+<template>
+    <navbar/>
+    <p>sevrerId: {{ serverId }}</p>
+    <p>channelId: {{ channelId }}</p>
+    <fusszeile/>
+</template>
+
+<script setup>
+const route = useRoute()
+</script>
+
+<script>
+export default {
+    data() {
+        return {
+            serverId: this.$route.params.servId,
+            channelId: this.$route.params.channelId
+        }
+    }
+}
+</script>
