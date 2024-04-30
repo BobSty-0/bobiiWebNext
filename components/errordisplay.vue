35cdb45fab9ec6b0e866d2f6003b480270c4df3d
BobSty <78264479+BobSty-0@users.noreply.github.com>
2024-04-30 23:33:46 +0200

    Error Page hinzugefügt

---
+++ components/errordisplay.vue
@@ -0,0 +1,24 @@
+<style lang="css" scoped>
+@import "../css/components/errordisplay.css";
+</style>
+
+<template>
+    <div class="error-container">
+        <div class="error-container-text">
+            <h2>Ooops something went wrong!</h2>
+            <h1>{{ code }}</h1>
+            <h3>{{ message }}</h3>
+
+            <NuxtLink to="/" ><button>Go back Home</button></NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        code: String,
+        message: String
+    },
+}
+</script>
---
+++ error.vue
@@ -0,0 +1,19 @@
+<template>
+    <navbar/>
+    <errordisplay :code="error.statusCode" :message="error.message"/>
+    <fusszeile/>
+</template>
+
+<script setup>
+    defineProps(['error'])
+</script>
+
+<script>
+import errordisplay from '/components/errordisplay.vue'
+
+export default {
+  components: {
+    errordisplay,
+  }
+};
+</script>
